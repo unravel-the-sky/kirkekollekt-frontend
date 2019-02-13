47f4d593e28e0248eced4257e988376325fa0ebb
Sadan Ekdemir <sadan.ekdemir@attensi.com>
2019-02-13 17:10:09 +0100

    UPDATE; WHOAA;

--- src/components/layout/Topbar.vue
+++ src/components/layout/Topbar.vue
@@ -1,10 +1,23 @@
 <template>
-  <div>
-      top bar here, wee
+  <div class="topbar-holder">
+    <img src="@/assets/imgs/kirkekollekt-header.png" class="logo-img">
+    <div>LOL</div>
   </div>
 </template>
 
 <style lang="scss" scoped>
+.topbar-holder {
+  padding: 0 $default-padding-small;
+  height: inherit;
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+
+  .logo-img {
+    width: 310px;
+    height: 80px;
+  }
+}
 </style>
 
 <script>
